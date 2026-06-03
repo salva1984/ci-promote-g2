93821c1d533adf09e7ab5d003c0b23698d54e8e5
Fernando Diaz <fndiaz@fiec.espol.edu.ec>
2026-06-03 14:12:51 -0500

    Init project

---
+++ src/cli.py
@@ -0,0 +1,18 @@
+import json
+import os
+
+archivo = "data.json"
+
+if not os.path.exists(archivo):
+    datos_iniciales = {
+        "books": [],
+        "members": [],
+        "loans": []
+    }
+
+    with open(archivo, "w", encoding="utf-8") as f:
+        json.dump(datos_iniciales, f, indent=4, ensure_ascii=False)
+
+    print("Se inicializó con la base de datos")
+else:
+    print("La base de datos ya existe.")
